b07209403d3f1134d2a3e9d9a17e60926519f183
Luis Anaya <luis.ernesto.anaya@upa.edu.mx>
2025-10-30 17:01:17 -0600

    Task program

--- 04 ADC_Reading/main/main.c
+++ 04 ADC_Reading/main/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h> // Incluye la biblioteca estándar de entrada/salida
 #include "freertos/FreeRTOS.h" // Incluye la biblioteca de FreeRTOS
 #include "esp_adc/adc_oneshot.h" // Incluye el controlador ADC en modo de una sola toma
+#include "esp_log.h"
+
+static const char *ESP = "Mi ESP";
+static const char *ADC = "Soy el ADC";
 
 int adc_value = 0; // Variable para almacenar el valor leído del ADC
 adc_oneshot_unit_handle_t adc1_handle; // Manejador para la unidad ADC1
@@ -15,10 +19,12 @@ void configuracion(void){
     adc_oneshot_new_unit(&init_config, &adc1_handle); // Inicializa el ADC
 
     adc_oneshot_chan_cfg_t channel_config = {
-        .atten = ADC_ATTEN_DB_11, // Atenuación de 11dB
+        .atten = ADC_ATTEN_DB_12, // Atenuación de 12dB
         .bitwidth = ADC_BITWIDTH_12, // Ancho de datos de 12 bits
     };
     adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_0, &channel_config); // Configura el canal 0 del ADC1
+    ESP_LOGE(ESP, "Ya termine la configuración \n");
+
 }
 
 void app_main(void)
@@ -32,12 +38,14 @@ void app_main(void)
         // Lee el valor crudo del canal 0 del ADC1
         esp_err_t ret = adc_oneshot_read(adc1_handle, ADC_CHANNEL_0, &adc_raw);
         if (ret != ESP_OK) {
-            printf("ADC read failed\n");
+           // printf("ADC read failed\n");
+            ESP_LOGE(ADC, "Ya valio\n");
             continue; // Si la lectura falla, continúa con la siguiente iteración
         }
         float voltage = adc_raw * (3.3 / 4095); // Convierte el valor ADC a voltaje (asumiendo Vref = 3.3V y resolución de 12 bits)
         // Imprime el valor leído por el ADC
-        printf("ADC Value is %d, Voltage is %.2fV\n", adc_raw, voltage);
+        printf("%d,%.2f\n", adc_raw, voltage);
+        ESP_LOGI(ADC, "Leyendo\n");
         vTaskDelay(pdMS_TO_TICKS(100)); // Espera 100 ms antes de la siguiente lectura
     }
 }
